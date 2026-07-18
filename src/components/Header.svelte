82d29c461833e2a2ddced49a82df54ab3537e74b
Florian Geierstanger <22551442+fgeierst@users.noreply.github.com>
2026-07-18 13:32:39 +0200

    feat: add meta description to Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,40 +1,44 @@
 <script>
-	import { base } from "$app/paths";
-	import Logo from "$lib/icons/Logo.svelte";
-	import Nav from "./Nav.svelte";
+  import { base } from "$app/paths";
+  import Logo from "$lib/icons/Logo.svelte";
+  import Nav from "./Nav.svelte";
 </script>
 
 <svelte:head>
-	<title>München Plantscht</title>
+  <title>München Plantscht</title>
+  <meta
+    name="description"
+    content="Real-time capacity level display of the Munich public swimming pools, with historical data"
+  />
 </svelte:head>
 
 <header>
-	<a href={base} class="title">
-		<div class="title__logo"><Logo /></div>
-		<h1 class="title__text">München Plantscht</h1>
-	</a>
+  <a href={base} class="title">
+    <div class="title__logo"><Logo /></div>
+    <h1 class="title__text">München Plantscht</h1>
+  </a>
 
-	<Nav />
+  <Nav />
 </header>
 
 <style>
-	.title {
-		display: flex;
-		color: var(--munich-black);
-		gap: 0.7rem;
-		margin-block-end: 0.5rem;
-		align-items: center;
-		text-decoration: none;
-		font-size: 200%;
-	}
+  .title {
+    display: flex;
+    color: var(--munich-black);
+    gap: 0.7rem;
+    margin-block-end: 0.5rem;
+    align-items: center;
+    text-decoration: none;
+    font-size: 200%;
+  }
 
-	.title__logo {
-		width: 1.6em;
-	}
+  .title__logo {
+    width: 1.6em;
+  }
 
-	.title__text {
-		font-size: inherit;
-		font-weight: inherit;
-		margin: 0;
-	}
+  .title__text {
+    font-size: inherit;
+    font-weight: inherit;
+    margin: 0;
+  }
 </style>
